3c39174b925906a271266d76ef94d1ff5f887cd2
Vlad Bezden <vlad.bezden@gmail.com>
2019-01-13 17:23:32 -0500

    added greedy algorithm

---
+++ greedy.py
@@ -0,0 +1,34 @@
+"""
+Example of greedy algorithm
+
+Find minimum number of stations that will cover most
+of the states
+"""
+
+
+states_needed = set(["mt", "wa", "or", "id", "nv", "ut", "ca", "az"])
+
+stations = {
+    "kone": set(["id", "nv", "ut"]),
+    "ktwo": set(["wa", "id", "mt"]),
+    "kthree": set(["or", "nv", "ca"]),
+    "kfour": set(["nv", "ut"]),
+    "kfive": set(["ca", "az"]),
+}
+
+final_stations = set()
+
+while states_needed:
+    best_station = None
+    states_covered = set()
+    for station, states in stations.items():
+        coverage = states_needed & states
+        if len(states_covered) < len(coverage):
+            states_covered = coverage
+            best_station = station
+
+    states_needed -= states_covered
+    final_stations.add(best_station)
+
+print(final_stations)
+# {'kone', 'kthree', 'kfive', 'ktwo'}
