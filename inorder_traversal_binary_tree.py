23754b9307540a6c5c39d0484682c82714157b58
Vlad Bezden <vlad.bezden@gmail.com>
2019-01-18 07:40:08 -0500

    added inorder traversal binary tree

---
+++ inorder_traversal_binary_tree.py
@@ -0,0 +1,50 @@
+"""
+In-order traversal of bynary tree
+
+              A
+             / \
+            B   C
+           / \   \
+          D   E   F
+         / \
+        G   H
+
+    G->D->H->B->E->A->F->C
+"""
+
+
+from __future__ import annotations
+from dataclasses import dataclass
+
+
+@dataclass
+class Node:
+    data: str
+    left: Node = None
+    right: Node = None
+
+
+@dataclass
+class Tree:
+    root: Node
+
+    def in_order(self, node: Node) -> None:
+        if not node:
+            return
+        self.in_order(node.left)
+        print(node.data, end="->")
+        self.in_order(node.right)
+
+
+if __name__ == "__main__":
+    h = Node("H")
+    g = Node("G")
+    f = Node("F")
+    e = Node("E")
+    d = Node("D", g, h)
+    c = Node("C", f)
+    b = Node("B", d, e)
+    a = Node("A", b, c)
+
+    tree = Tree(a)
+    tree.in_order(a)
