75e66b2bd4c062a7b3dcfc0609057542ca046d75
Vlad Bezden <vlad.bezden@gmail.com>
2019-01-19 06:41:23 -0500

    added __repr__ and in_order traverse

--- bst.py
+++ bst.py
@@ -4,6 +4,7 @@ Basic example of BST
 
 from __future__ import annotations
 from dataclasses import dataclass
+from typing import List
 import random
 
 
@@ -59,10 +60,32 @@ class Tree:
             current = current.right
         return current
 
+    def in_order(self, node: Node) -> List[str]:
+        """
+        Process left sub tree
+        Current tree
+        Process right sub tree
+        """
+        if node.left:
+            for n in self.in_order(node.left):
+                yield n
+        yield node
+        if node.right:
+            for n in self.in_order(node.right):
+                yield n
+
+    def __repr__(self):
+        """In-order tree print"""
+        trace = []
+        for n in self.in_order(self.root):
+            trace.append(n.value)
+
+        return "->".join(map(str, trace))
+
 
 if __name__ == "__main__":
     MAX_VAL = 10 ** 3
-    NODES = 100
+    NODES = 20
     root_value = random.randint(0, MAX_VAL)
     tree = Tree(Node(root_value))
     for i in random.sample(range(MAX_VAL), NODES):
@@ -71,3 +94,5 @@ if __name__ == "__main__":
     print(f"Root value: {tree.root.value}")
     print(f"min tree value {tree.min.value}")
     print(f"max tree value {tree.max.value}")
+
+    print(tree)
