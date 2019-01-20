48c51f379252cb68060bc2220407f4acb705fc21
Vlad Bezden <vlad.bezden@gmail.com>
2019-01-20 12:20:47 -0500

    added EmptyHeapErrorand converted dataclass to regular class

--- heap.py
+++ heap.py
@@ -2,27 +2,31 @@
 Heap (min-heap) implementation example
 """
 
-from dataclasses import dataclass
 from random import sample
 
 
 HEAP_SIZE = 20
 
 
-@dataclass
+class EmptyHeapError(Exception):
+    def __init__(self):
+        super().__init__("No elements in the heap")
+
+
 class Heap:
-    heap = [0]
+    def __init__(self):
+        self.__heap = [0]
 
     @property
     def size(self) -> int:
-        return len(self.heap) - 1
+        return len(self.__heap) - 1
 
     @property
     def is_empty(self) -> bool:
         return self.size <= 0
 
     def insert(self, item: int) -> None:
-        self.heap.append(item)
+        self.__heap.append(item)
         self.arrange(self.size)
 
     def __iter__(self):
@@ -32,7 +36,7 @@ class Heap:
     def __next__(self):
         self.__current += 1
         if self.__current < self.size:
-            return self.heap[self.__current]
+            return self.__heap[self.__current]
         else:
             raise StopIteration
 
@@ -41,10 +45,10 @@ class Heap:
         parent_index = index // 2
         if parent_index <= 0:
             return
-        if self.heap[index] < self.heap[parent_index]:
-            self.heap[index], self.heap[parent_index] = (
-                self.heap[parent_index],
-                self.heap[index],
+        if self.__heap[index] < self.__heap[parent_index]:
+            self.__heap[index], self.__heap[parent_index] = (
+                self.__heap[parent_index],
+                self.__heap[index],
             )
         self.arrange(parent_index)
 
@@ -58,38 +62,38 @@ class Heap:
         r_index = l_index + 1
         if self.size < r_index:
             return l_index
-        return l_index if self.heap[l_index] < self.heap[r_index] else r_index
+        return l_index if self.__heap[l_index] < self.__heap[r_index] else r_index
 
     def sink(self, index: int) -> None:
         """Rebalances the tree when item is popped"""
         child_index = self._child_min_index(index)
         if self.size < child_index:
             return
-        if self.heap[child_index] < self.heap[index]:
-            self.heap[index], self.heap[child_index] = (
-                self.heap[child_index],
-                self.heap[index],
+        if self.__heap[child_index] < self.__heap[index]:
+            self.__heap[index], self.__heap[child_index] = (
+                self.__heap[child_index],
+                self.__heap[index],
             )
         self.sink(child_index)
 
     def pop(self) -> int:
         """Pop root (lowest element) of the tree"""
         if self.is_empty:
-            return None
-        item = self.heap[1]
+            raise EmptyHeapError()
+        item = self.__heap[1]
         # get latest element in the heap and put it to the root of the tree
-        last_item = self.heap.pop()
+        last_item = self.__heap.pop()
         if not self.is_empty:
-            self.heap[1] = last_item
+            self.__heap[1] = last_item
             # rebalance a tree
             self.sink(1)
         return item
 
     def clear(self):
-        self.heap = [0]
+        self.__heap = [0]
 
     def __repr__(self):
-        return f"[{', '.join(map(str, self.heap[1:]))}]"
+        return f"[{', '.join(map(str, self.__heap[1:]))}]"
 
 
 def main():
@@ -117,6 +121,12 @@ def main():
     while not heap.is_empty:
         item = heap.pop()
         print(f"item: {item}, heap: {heap}")
+
+    try:
+        heap.pop()
+        assert False, "Failed. There are no elements in the heap"
+    except EmptyHeapError as ex:
+        print(f"Exeption: {ex}")
 
 
 if __name__ == "__main__":
