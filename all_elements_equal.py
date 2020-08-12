271e804c8b4cef944a4d918ac1da0f8f8be2b470
Vlad Bezden <vlad.bezden@gmail.com>
2020-08-12 10:06:07 -0400

    Refactor 'all_elements_equal'

--- all_elements_equal.py
+++ all_elements_equal.py
@@ -4,34 +4,29 @@ Check if all elements in a list are the equal.
 Output:
     using_set took: 0.332290
     using_compare took: 0.220397
-    using_sort took: 2.083070
 """
 
+from typing import List
 import numpy as np
 from timeit import timeit
 
 
-data = np.random.randint(1, 100, 1_000_000).tolist()
+data:List[int] = np.random.randint(1, 100, 1_000_000).tolist()
 print(f"{len(data) = }")
 
 
-def using_set(data):
+def using_set(data:List[int]) -> bool:
     return len(set(data)) <= 1
 
 
-def using_compare(data):
+def using_compare(data:List[int]) -> bool:
     return data[1:] == data[:-1]
-
-
-def using_sort(data):
-    return data[0] == sorted(data)[-1]
 
 
 if __name__ == "__main__":
     for f in [
         using_set,
         using_compare,
-        using_sort,
     ]:
         t = timeit(stmt=f"f({data})", number=10, globals=globals())
         print(f"{f.__name__} took: {t:.6f}")
