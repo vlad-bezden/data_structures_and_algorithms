21aa061a4e5ed37a288265500e7d34e92d61056e
Vlad Bezden <vlad.bezden@gmail.com>
2020-08-22 07:14:39 -0400

    Improve sieve algorithm and add performance tests

--- data_structures_and_algorithms/prime_generator.py
+++ data_structures_and_algorithms/prime_generator.py
@@ -2,11 +2,11 @@
 
     Output:
 
-    classical_prime_gen(1000). Exec time = 0.002481 sec
-    sieve_prime_gen(1000). Exec time = 0.002761 sec
+    classical_prime_gen(1000). Exec time = 0.000208 sec
+    sieve_prime_gen(1000). Exec time = 0.000162 sec
 
-    classical_prime_gen(100000). Exec time = 0.002602 sec
-    sieve_prime_gen(100000). Exec time = 0.002768 sec
+    classical_prime_gen(100000). Exec time = 0.000112 sec
+    sieve_prime_gen(100000). Exec time = 0.000115 sec
 """
 
 from math import sqrt
@@ -21,16 +21,20 @@ class Test:
     expected: int
 
 
-TESTS = [Test(1_000, 168), Test(100_000, 9592)]
+TESTS = [Test(1_000, 168), Test(1_000_000, 78498)]
 
 
 def sieve_prime_gen(num: int) -> Generator[int, None, None]:
     """Based on Sieve algorithm."""
-    candidates = [*range(2, num + 1)]
-
-    while candidates and (candidate := candidates[0]):
-        yield candidate
-        candidates = [n for n in candidates if n % candidate != 0]
+    candidates = [False, False] + [True] * (num - 1)
+    for p in range(2, int(sqrt(num) + 1)):
+        # if we haven't already eliminated p as a prime
+        if candidates[p]:
+            # eliminate all multiples of p, starting at p ** 2
+            for m in range(p * p, num + 1, p):
+                candidates[m] = False
+    # return the indices that weren't eliminated
+    yield from (n for n, prime in enumerate(candidates) if prime)
 
 
 def classical_prime_gen(num: int) -> Generator[int, None, None]:
@@ -47,8 +51,8 @@ def classical_prime_gen(num: int) -> Generator[int, None, None]:
 def validate(funcs: Sequence[Callable[[int], Generator[int, None, None]]]) -> None:
     for test in TESTS:
         for f in funcs:
-            result = list(f(test.data))
-            assert len(result) == test.expected, f"{f.__name__}({test}), {result = }"
+            result = len([*f(test.data)])
+            assert result == test.expected, f"{f.__name__}({test}), {result = }"
     print("PASSED!!!\n")
 
 
@@ -58,7 +62,10 @@ if __name__ == "__main__":
     for test in TESTS:
         for f in funcs:
             t = repeat(
-                stmt=f"f({test.data})", repeat=10, number=10_000, globals=globals()
+                stmt=f"{len([*f(test.data)])}",
+                repeat=5,
+                number=100_000,
+                globals=globals(),
             )
             print(f"{f.__name__}({test.data}). Exec time = {min(t):.6f} sec")
         print()
